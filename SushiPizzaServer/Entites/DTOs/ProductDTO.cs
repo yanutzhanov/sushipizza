fa80513fda729c078c470d97419d79af395931a0
Jandere <iutzhanov@bk.ru>
2020-06-17 23:26:34 +0600

    [add] ProductDTO Validation

--- SushiPizzaServer/Entites/DTOs/ProductDTO.cs
+++ SushiPizzaServer/Entites/DTOs/ProductDTO.cs
@@ -1,11 +1,23 @@
-﻿namespace Entites.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entites.DTOs
 {
     public class ProductDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Название продукта обязательно")]
+        [StringLength(30, ErrorMessage = "Длина названия не может быыть больше 30 символов")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Содержимое обязательно")]
+        [StringLength(120, ErrorMessage = "Длина состава не может быть больше 120 символов")]
         public string Composition { get; set; }
+
+        [Required(ErrorMessage = "Бесплатный напиток? Серьезно?")]
         public double Price { get; set; }
+
+        [Required(ErrorMessage = "Тип продукта обязателен")]
+        [StringLength(10, ErrorMessage = "Все типы меньше 10 символов, серьезно")]
         public string Type { get; set; }
     }
 }
--- SushiPizzaServer/Entites/DTOs/ProductForUpdateDTO.cs
+++ SushiPizzaServer/Entites/DTOs/ProductForUpdateDTO.cs
@@ -1,10 +1,22 @@
-﻿namespace Entites.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entites.DTOs
 {
     public class ProductForUpdateDTO
     {
+        [Required(ErrorMessage = "Название продукта обязательно")]
+        [StringLength(30, ErrorMessage = "Длина названия не может быыть больше 30 символов")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Содержимое обязательно")]
+        [StringLength(120, ErrorMessage = "Длина состава не может быть больше 120 символов")]
         public string Composition { get; set; }
+
+        [Required(ErrorMessage = "Бесплатный напиток? Серьезно?")]
         public double Price { get; set; }
+
+        [Required(ErrorMessage = "Тип продукта обязателен")]
+        [StringLength(10, ErrorMessage = "Все типы меньше 10 символов, серьезно")]
         public string Type { get; set; }
     }
 }
