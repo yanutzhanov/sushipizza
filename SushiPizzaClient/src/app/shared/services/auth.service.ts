76d8c9ebab7b13e1aad1ca3c26289d74b543e3d5
Jandere <iutzhanov@bk.ru>
2020-06-22 05:58:04 +0600

    [update] AuthService

--- SushiPizzaClient/src/app/shared/services/auth.service.ts
+++ SushiPizzaClient/src/app/shared/services/auth.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { RepositoryService } from './repository.service';
+import { User } from 'src/app/interfaces/user.model';
+import { UserForCreation } from 'src/app/interfaces/user-for-creation.model';
+import { UserLoginModel } from 'src/app/interfaces/user-login-model.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +13,7 @@ export class AuthService {
   public isAuthorizedObs: BehaviorSubject<boolean>;
   public user: User;
 
-  constructor(private repo: RepositoryService) {
+  constructor(private repo: RepositoryService, private router: Router) {
     this.isAuthorizedObs = new BehaviorSubject<boolean>(false);
     if (sessionStorage.getItem('token')) {
       this.repo.getData('api/users/account', true).subscribe(
@@ -29,40 +33,12 @@ export class AuthService {
 
   signUp = (user: UserForCreation) => {
     const apiUrl = 'api/users/register';
-    this.repo.create(apiUrl, user, false).subscribe(
-      res => {
-        sessionStorage.setItem('token', (res as RegisterResponse).jwtToken);
-        sessionStorage.setItem('role', (res as RegisterResponse).role);
-        console.log(res);
-        this.repo.getData('api/users/account', true).subscribe(
-          userInfo => {
-            this.user = userInfo as User;
-            this.setAuth(true);
-          },
-          err => console.error(err)
-        );
-      },
-      err => console.error(err)
-    );
+    this.getToken(apiUrl, user);
   }
 
   signIn = (user: UserLoginModel) => {
     const apiUrl = 'api/users/login';
-    this.repo.create(apiUrl, user, false).subscribe(
-      res => {
-        sessionStorage.setItem('token', (res as RegisterResponse).jwtToken);
-        sessionStorage.setItem('role', (res as RegisterResponse).role);
-        console.log(res);
-        this.repo.getData('api/users/account', true).subscribe(
-          userInfo => {
-            this.user = userInfo as User;
-            this.setAuth(true);
-          },
-          err => console.error(err)
-        );
-      },
-      err => console.error(err)
-    );
+    this.getToken(apiUrl, user);
   }
 
   signOut = () => {
@@ -70,30 +46,34 @@ export class AuthService {
     sessionStorage.removeItem('role');
     this.setAuth(false);
     this.user = null;
+    this.router.navigate(['/signin']);
+  }
+
+  private getToken(apiUrl: string, body: UserForCreation | UserLoginModel) {
+    this.repo.create(apiUrl, body, false).subscribe(
+      res => {
+        sessionStorage.setItem('token', (res as RegisterResponse).jwtToken);
+        sessionStorage.setItem('role', (res as RegisterResponse).role);
+        console.log(res);
+        this.getUserInfo();
+      },
+      err => console.error(err)
+    );
+  }
+
+  private getUserInfo() {
+    this.repo.getData('api/users/account', true).subscribe(
+      userInfo => {
+        this.user = userInfo as User;
+        this.setAuth(true);
+        this.router.navigate(['/account']);
+      },
+      err => console.error(err)
+    );
   }
 }
 
-export interface UserForCreation {
-  fullName: string;
-  phoneNumber: string;
-  address: string;
-  password: string;
-  passwordConfirm: string;
-}
-
-export interface UserLoginModel {
-  phoneNumber: string;
-  password: string;
-}
-
-export interface RegisterResponse {
+interface RegisterResponse {
   jwtToken: string;
   role: string;
-}
-
-interface User {
-  id: number;
-  fullName: string;
-  phoneNumber: string;
-  address: string;
 }
